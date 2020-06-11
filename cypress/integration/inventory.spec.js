b0a2380b94e3d0656ed675f5fa2ec8b86a0991fb
jacekzygiel <jacekzygiel@gmail.com>
2020-06-11 22:28:44 +0200

    Adds project structure, adds tests for each functionality

---
+++ cypress/integration/inventory.spec.js
@@ -0,0 +1,22 @@
+context("Inventory", () => {
+
+    beforeEach(() => {
+        cy.setSessionStorage('cart-contents', '[]')
+        cy.visit("/inventory.html")
+    })
+
+    describe("Cart", () => {
+        it("Add item to cart", () => {
+            cy.get('.inventory_list .inventory_item')
+                .first().as('firstItem')
+
+            cy.get('@firstItem')
+                .find('.btn_primary')
+                .click()
+
+            cy.get('.shopping_cart_badge').should('have.text', '1')
+            cy.getSessionStorage('cart-contents').should("eq", '[4]')
+        })
+
+    })
+})
---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,23 @@
+context('Login', () => {
+    let usersData
+
+    before(() => {
+        cy.fixture('users').then(($users) => usersData = $users)
+    })
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    describe("Registered user", () => {
+        it("can login", () => {
+            let user = usersData.standard_user
+            cy.visit("/")
+            cy.login(user.login, user.password)
+            cy.url()
+                .should('contain', 'inventory')
+            cy.get('.product_label')
+                .should('be.visible')
+        })
+    })
+})
